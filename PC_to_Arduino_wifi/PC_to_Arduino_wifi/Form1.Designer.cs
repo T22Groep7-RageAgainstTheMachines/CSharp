3fe96f6e35847657caf758fe0f6562355c9619c9
charmesal <kevindullens3@hotmail.com>
2016-10-06 11:00:27 +0200

    Uploaded Mark's work

---
+++ PC_to_Arduino_wifi/PC_to_Arduino_wifi/Form1.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace PC_to_Arduino_wifi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxTextToSend = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Location = new System.Drawing.Point(12, 104);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(260, 83);
+            this.buttonSend.TabIndex = 0;
+            this.buttonSend.Text = "Send Message";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonHello_Click);
+            // 
+            // textBoxTextToSend
+            // 
+            this.textBoxTextToSend.Location = new System.Drawing.Point(12, 12);
+            this.textBoxTextToSend.Multiline = true;
+            this.textBoxTextToSend.Name = "textBoxTextToSend";
+            this.textBoxTextToSend.Size = new System.Drawing.Size(260, 68);
+            this.textBoxTextToSend.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.textBoxTextToSend);
+            this.Controls.Add(this.buttonSend);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxTextToSend;
+    }
+}
+
---
+++ PC_to_Arduino_wifi/PC_to_Arduino_wifi/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Sockets;
+using System.Net;
+
+
+namespace PC_to_Arduino_wifi
+{
+    public partial class Form1 : Form
+    {
+        Socket sock;
+        IPAddress serverAddr;
+        IPEndPoint endPoint;
+        public Form1()
+        {
+            InitializeComponent();
+            sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+            serverAddr = IPAddress.Parse("192.168.0.131");
+            endPoint = new IPEndPoint(serverAddr, 2390);
+
+
+        }
+
+        private void buttonHello_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(textBoxTextToSend.Text))
+            {
+                byte[] send_buffer = Encoding.ASCII.GetBytes(textBoxTextToSend.Text);
+                sock.SendTo(send_buffer, endPoint);
+            }
+
+        }
+
+
+
+    }
+}
